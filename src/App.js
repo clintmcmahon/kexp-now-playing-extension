675db8204fd9a1a36bb5cd893a8b73032eb67e94
Clint McMahon <clint@cmcmahon.io>
2021-05-30 17:18:11 -0500

    updated design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [nowPlaying, setNowPlaying] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.kexp.org/v2/plays/")
+      .then((response) => response.json())
+      .then(data => {
+        let results = data.results;
+        let nowPlaying = results[0];
+        setNowPlaying(nowPlaying);
+      })
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="wrapper">
+          {nowPlaying && 
+          <>
+          <div className="left">
+              <img src={nowPlaying.thumbnail_uri} alt={nowPlaying.album} />
+          </div>
+          <div className="right">
+            <div className="block">
+              <div className="label">
+                Artist:
+        </div>
+              <div className="text">
+                {nowPlaying.artist}
+              </div>
+            </div>
+            <div className="block">
+              <div className="label">
+                Song:
+            </div>
+              <div className="text">
+                {nowPlaying.song}
+              </div>
+            </div>
+            <div className="block">
+              <div className="label">
+                Album:
+            </div>
+              <div className="text">
+                {nowPlaying.album}
+              </div>
+            </div>
+          </div>
+          </>
+}
+        </div>
   );
 }
 
